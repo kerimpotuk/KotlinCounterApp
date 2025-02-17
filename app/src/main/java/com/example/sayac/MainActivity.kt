72e690d3e2f95bfc41791ddc5477a47049ebebe4
kerimpotuk@gmail.com <kerimpotuk@gmail.com>
2025-02-17 17:13:54 +0300

    Counter Project first commit

---
+++ app/src/main/java/com/example/sayac/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.sayac
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import android.animation.ObjectAnimator
+import android.view.View
+
+
+class MainActivity : ComponentActivity() {
+    private lateinit var txtCounter: TextView
+    private lateinit var editTextAmount: EditText
+    private var counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        txtCounter = findViewById(R.id.txtCounter)
+        editTextAmount = findViewById(R.id.editTextAmount)
+        val btnIncrease: Button = findViewById(R.id.btnIncrease)
+        val btnReset: Button = findViewById(R.id.btnReset)
+
+        btnIncrease.setOnClickListener {
+            // Animasyonu uygula
+            animateButton(btnIncrease)
+
+            val amountString = editTextAmount.text.toString()
+            if (amountString.isNotEmpty()) {
+                val amount = amountString.toInt()
+                counter += amount
+                txtCounter.text = counter.toString()
+            }else{
+                counter++
+                txtCounter.text = counter.toString()
+            }
+        }
+
+        btnReset.setOnClickListener {
+            animateButton(btnReset)
+            counter = 0
+            txtCounter.text = counter.toString()
+            editTextAmount.text.clear() // Giriş alanını temizle
+        }
+    }
+
+    private fun animateButton(view: View) {
+        // X ekseninde ölçeklendirme animasyonu
+        val scaleX = ObjectAnimator.ofFloat(view, "scaleX", 1f, 1.2f, 1f)
+        // Y ekseninde ölçeklendirme animasyonu
+        val scaleY = ObjectAnimator.ofFloat(view, "scaleY", 1f, 1.2f, 1f)
+
+        // Animasyon süresi
+        scaleX.duration = 200
+        scaleY.duration = 200
+
+        // Animasyonları başlat
+        scaleX.start()
+        scaleY.start()
+    }
+}
